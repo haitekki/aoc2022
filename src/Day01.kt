3402c7942fa33a7d9a312c29a49480035edd40f6
Juho Tervo <juho.tervo@solita.fi>
2022-12-01 14:22:23 +0200

    day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,19 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun part1(input: String): Int {
+        return input.split(System.lineSeparator()+System.lineSeparator()).map {
+            it.lines().map { number ->
+                number.toInt()
+            }.sum()
+        }.maxOrNull()?: 0
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input:String): Int {
+        return input.split(System.lineSeparator()+System.lineSeparator()).map {
+            it.lines().map { number ->
+                number.toInt()
+            }.sum()
+        }.sortedDescending().subList(0,3).sum()
     }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
 
     val input = readInput("Day01")
     println(part1(input))
